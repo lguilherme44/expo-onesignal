5b13abbee264e4642eec16bdaafbbe495535c0c3
Guilherme Lellis <lguilherme44@gmail.com>
2021-12-07 23:02:11 -0300

    expo onesignal description

--- App.js
+++ App.js
@@ -42,7 +42,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Expo Onesignal</Text>
       <StatusBar style="auto" />
     </View>
   );
